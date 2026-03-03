f02dfe333a4535a4fd8a06a2ff0be35d4713afe2
Асель Абдрахманова <dellya4>
2026-03-03 10:36:25 +0500

    fix: change Status, create ENUM

--- src/Task.java
+++ src/Task.java
@@ -4,34 +4,42 @@ public class Task {
     private Long id;
     private String Title;
     private String Description;
-    private String Status;
+    private Status Status;
 
     public Task() {}
 
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getTitle() {
         return Title;
     }
+
     public void setTitle(String title) {
         Title = title;
     }
+
     public String getDescription() {
         return Description;
     }
+
     public void setDescription(String description) {
         Description = description;
     }
-    public String getStatus() {
+
+    public Status getStatus() {
         return Status;
     }
-    public void setStatus(String status) {
+
+    public void setStatus(Status status) {
         Status = status;
     }
+
     @Override
     public String toString() {
         return "id = " + id + ", title = " + Title + ", description = " + Description + ", status = " + Status;
