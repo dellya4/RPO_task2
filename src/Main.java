5f1fd4faac387b413419eee1addc63e2e70d9f49
Асель Абдрахманова <dellya4>
2026-03-09 23:26:52 +0500

    feat: create loop for menu

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,61 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        TaskManager taskManager = new TaskManager();
+        FileManager fileManager = new FileManager("tasks.csv");
 
-        System.out.print("Enter title: ");
-        String title = input.nextLine();
+        TaskManager taskManager = new TaskManager(fileManager.loadTasks());
 
-        System.out.print("Enter description: ");
-        String description = input.nextLine();
+        boolean running = true;
+        while (running) {
+            System.out.println("    Task Manager    ");
+            System.out.println("1 - add task\n2 - list tasks\n3 - complete task\n4 - delete task\n5 - save\n6 - exit");
+            System.out.print("Enter your choice: ");
 
-        Task task = new Task();
-        task.setTitle(title);
-        task.setDescription(description);
+            String choice = input.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.print("Enter title: ");
+                    String title = input.nextLine();
+                    System.out.print("Enter description: ");
+                    String description = input.nextLine();
 
-        taskManager.addTask(task);
+                    Task task = new Task();
+                    task.setTitle(title);
+                    task.setDescription(description);
+
+                    taskManager.addTask(task);
+                    break;
+                case "2":
+                    taskManager.listTasks();
+                    break;
+                case "3":
+                    System.out.println("Enter task id: ");
+                    long completeId = Long.parseLong(input.nextLine());
+                    taskManager.completedTask(completeId);
+                    System.out.println("Task completed successfully");
+                    break;
+                case "4":
+                    System.out.println("Enter task id: ");
+                    long deleteId = Long.parseLong(input.nextLine());
+                    taskManager.deleteTask(deleteId);
+                    System.out.println("Task has been deleted.");
+                    break;
+                case "5":
+                    fileManager.saveTasks(taskManager.getTasks());
+                    System.out.println("Task list has been saved");
+                    break;
+                case "6":
+                    System.out.println("Goodbye!");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+            }
+        }
+
+
+
+
+
     }
 }
